acd4c7c13ad52cbda5d4fd7170aa6bb42eadf82e
thiago <thiagoe99@hotmail.com>
2022-08-10 16:31:38 -0300

    adicionacomentarios

--- Startup.cs
+++ Startup.cs
@@ -58,6 +58,7 @@ namespace configurando_QuickStartUI_IS4
             var connectionString = Configuration.GetConnectionString("Configuration");
             var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
 
+            //preconfiguracoes do IdentityServer4
             var builder = services.AddIdentityServer(options =>
                 {
                     options.Events.RaiseErrorEvents = true;
@@ -93,6 +94,7 @@ namespace configurando_QuickStartUI_IS4
                 });
 
             // not recommended for production - you need to store your key material somewhere secure
+            //adiciona a chave de criptografia padrao
             builder.AddDeveloperSigningCredential();
 
             services.AddAuthentication()
