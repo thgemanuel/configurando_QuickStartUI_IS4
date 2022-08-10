f2dfdb59a462fce5585df2cebc7a59717e53377a
thiago <thiagoe99@hotmail.com>
2022-08-10 17:29:24 -0300

    recriando projeto

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace configurando_QuickStartUI_IS4.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
