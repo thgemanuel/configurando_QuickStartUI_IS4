b4d42a09cbebcb8a1bbe0eefd2558de35eea37b5
thiago <thiagoe99@hotmail.com>
2022-08-10 16:18:55 -0300

    modelo MVC criado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace configurando_QuickStartUI_IS4.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
